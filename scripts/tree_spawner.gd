670e3e6e6c9d233cdd1bd8500d3c89618ed0e2cb
Cyvern <cyverneon@gmail.com>
2025-03-22 01:59:35 +0000

    Import a bark material and use it for spawned tree

--- scripts/tree_spawner.gd
+++ scripts/tree_spawner.gd
@@ -2,6 +2,7 @@ extends Node
 
 func spawn_trees():
 	var tree = Tree3D.new()
+	tree.material_trunk = load("res://materials/bark_material.tres")
 	add_child(tree)
 
 # Called when the node enters the scene tree for the first time.
