ec48c98f360f6c94071ce76b0c96f3bcbf72c80e
Cyvern <cyverneon@gmail.com>
2025-03-22 15:22:16 +0000

    Make tree spawner create a tree with randomized branches count at the position of each child Node3D

---
+++ scripts/param_ranges.gd
@@ -0,0 +1,4 @@
+extends Resource
+
+@export var branches_count_min : int = 3
+@export var branches_count_max : int = 5
--- scripts/tree_spawner.gd
+++ scripts/tree_spawner.gd
@@ -1,10 +1,33 @@
 extends Node
 
-func spawn_trees():
-	var tree = Tree3D.new()
-	tree.material_trunk = load("res://materials/bark_material.tres")
-	add_child(tree)
+var group : int = 2
 
-# Called when the node enters the scene tree for the first time.
+const param_ranges = preload("param_ranges.gd")
+var params : param_ranges
+
+func create_tree(pos : Vector3):
+	var tree = Tree3D.new()
+	
+	# fixed parameters
+	tree.trunk_segments = 8
+	tree.material_trunk = load("res://materials/bark_material.tres")
+	tree.twig_enable = false # TEMP FOR DEBUGGING
+	tree.position = pos
+	
+	# randomized parameters
+	tree.trunk_branches_count = randi_range(params.branches_count_min, params.branches_count_max)
+
+	return tree
+
+func spawn_trees():
+	for child in get_children():
+		if child is Node3D:
+			add_child(create_tree(child.position))
+
 func _ready():
+	if group == 1:
+		params = load("res://resources/param_ranges_group1.tres")
+	else:
+		params = load("res://resources/param_ranges_group2.tres")
+		
 	spawn_trees()
