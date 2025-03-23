460e788c0970a4e1ead1036d4af20dced306991b
Cyvern <cyverneon@gmail.com>
2025-03-23 03:33:24 +0000

    Start implementing photo mechanic

--- scripts/photo_cam.gd
+++ scripts/photo_cam.gd
@@ -1,7 +1,17 @@
 extends Node
 
 @export_group("References")
-@export var cam : Camera3D
+@export var texture_rect : TextureRect
+@export var viewport : Viewport
+
+func take_photo():
+	var image = viewport.get_texture().get_image()
+	var texture = ImageTexture.create_from_image(image)
+	texture_rect.set_texture(texture)
+
+func _input(event):
+	if event.is_action_pressed("photo"):
+		take_photo()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
